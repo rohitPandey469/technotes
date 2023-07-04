922f4f733b2132015b1f27a46d369f66f9696628
rohitPandey469 <rd8614196@gmail.com>
2023-07-04 21:20:09 +0530

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-api-9si5.onrender.com",
+  baseUrl: "https://technotes-api-o74k.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     // api.getState
